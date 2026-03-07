420bea3b3e518c5624c2d7c2fcdc41fbf8afe28f
estevesdev <sauloestevesdealmeida27@gmail.com>
2026-03-07 00:28:24 -0400

    setup: install and configure react-hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
 import Dashboard from './pages/Dashboard';
 import PrivateRoute from './PrivateRoute'; // Verifique se o import está correto
+import { Toaster } from 'react-hot-toast';
 
 
 function App() {
   return (
     <Router>
+      <Toaster position="top-right" reverseOrder={false} />
       <Routes>
         <Route path="/" element={<Login />} />
 
